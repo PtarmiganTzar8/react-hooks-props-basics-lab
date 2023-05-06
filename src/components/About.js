a0c029cc317e7d770d23cdfabf150bfbc4b58a8c
Michael <michael.d.lange88@gmail.com>
2023-05-05 19:26:58 -0700

    commit tree ad6d74069fff9a71a3636aecc5a3cd380077e804

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,23 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+  // console.log(props)
+  // function bioSeen() {
+  //   if (props.bio && props.bio.length > 0) {
+  //     return <p>{props.bio}</p>
+  //   } else {
+  //     return null
+  //   }
+  // }
+
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {props.bio && props.bio.length > 0 ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.github} linkedin={props.linkedin}/>
     </div>
   );
 }
